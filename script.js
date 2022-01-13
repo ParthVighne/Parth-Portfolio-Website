82b84554cd06136f0bd4bda77857245037bea22c
Parth Vighne <89728726+ParthVighne@users.noreply.github.com>
2022-01-13 13:25:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-//javascript for navigation bar effects on scroll
+// Javascript for navigation bar effects on scroll.
+
 window.addEventListener("scroll", function(){
   const header = document.querySelector("header");
   header.classList.toggle('sticky', window.scrollY > 0);
 });
 
-//javascript for responsive navigation sidebar menu
+// Javascript for responsive navigation sidebar menu.
+
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
 const navigationItems = document.querySelectorAll(".navigation a")
@@ -21,20 +23,23 @@ navigationItems.forEach((navigationItem) => {
   });
 });
 
-//javascript for scroll to top button
+// Javascript for scroll to top button.
+
 const scrollBtn = document.querySelector(".scrollToTop-btn");
 
 window.addEventListener("scroll", function(){
   scrollBtn.classList.toggle("active", window.scrollY > 500);
 });
 
-//javascript for scroll back to top on click scroll-to-top button
+// javascript for scroll back to top on click scroll-to-top button.
+
 scrollBtn.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
 
-//javascript for reveal website elements on scroll
+// Javascript for reveal website elements on scroll.
+
 window.addEventListener("scroll", reveal);
 
 function reveal(){
